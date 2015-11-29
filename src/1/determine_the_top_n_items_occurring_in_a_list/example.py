4aa02aca8c34ef2a2b573d0cec4347617fb527c5
frankyym <frankyym@gmail.com>
2015-11-29 12:04:21 +0100

    More understanding of Standard Library `collections`

--- src/1/determine_the_top_n_items_occurring_in_a_list/example.py
+++ src/1/determine_the_top_n_items_occurring_in_a_list/example.py
@@ -9,17 +9,17 @@ words = [
    'my', 'eyes', "you're", 'under'
 ]
 
-from collections import Counter
-word_counts = Counter(words)
+# This module implements specialized container datatypes providing alternatives
+# to Python’s general purpose built-in containers, dict, list, set, and tuple.
+# Reference: https://docs.python.org/2/library/collections.html
+import collections
+
+word_counts = collections.Counter(words)
 top_three = word_counts.most_common(3)
 print(top_three)
 # outputs [('eyes', 8), ('the', 5), ('look', 4)]
 
 # Example of merging in more words
-
 morewords = ['why','are','you','not','looking','in','my','eyes']
 word_counts.update(morewords)
 print(word_counts.most_common(3))
-
-
-
